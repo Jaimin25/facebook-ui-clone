e329df800d86ad392c7ffbbd7f85719d5b0ad168
jaimin25 <jaiminchovatia25123@gmail.com>
2023-09-17 21:03:40 +0530

    UI Changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ function App() {
 
   return (
     <div>
-      <div className='container flex sm:flex-col md:flex-col lg:flex-row mx-auto mt-28 items-center justify-center'>
-        <div className='left mx-14 w-4/5 mb-6'>
+      <div className='container flex sm:flex-col md:flex-row lg:flex-row mt-20 items-center justify-center'>
+        <div className='left ml-36 w-4/5 mb-6 md:flex-col md:justify-center md:items-center'>
           <img className='w-60' src='./src/assets/fb.svg'></img>
           <p className='text-2xl mx-6'>Facebook helps you connect and share with the people in your life.</p>
         </div>
-        <div className='right flex flex-col bg-white p-8 rounded-lg shadow-lg items-center w-[396px] mr-10 relative'>
+        <div className='right flex flex-col bg-white p-8 rounded-lg shadow-lg items-center w-[396px] mr-40 relative'>
           <input className='text-[17px] w-[364px] pt-[14px] pb-[14px] pr-4 pl-4 mt-3 border-[#dddfe2] border-2 rounded-md' type='email' placeholder='Email address or phone number'></input>
           <input className='text-[17px] w-[364px] pt-[14px] pb-[14px] pr-4 pl-4 mt-3 border-[#dddfe2] border-2 rounded-md' type='password' placeholder='Password'></input>
           <button className='text-xl bg-[#1877f2] rounded-md text-white w-[364px] leading-[48px] my-4 font-bold'>Log in</button>
